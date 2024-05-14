efeff82ec039694cf4834e227aa038a8cfcba468
rinox3005 <peppe.rinox@gmail.com>
2024-05-14 16:35:27 +0200

    [UPD] milestone 1 done

--- js/main.js
+++ js/main.js
@@ -1,43 +1,5 @@
 'use strict';
 
-// oggetti membri team
-// const wayneBarnett = {
-//     name: 'Wayne Barnett',
-//     role: 'Founder & CEO',
-//     image: 'wayne-barnett-founder-ceo.jpg',
-// }
-
-// const angelaCaroll = {
-//     name: 'Angela Caroll',
-//     role: 'Chief Editor',
-//     image: 'angela-caroll-chief-editor.jpg',
-// }
-
-// const walterGordon = {
-//     name: 'Walter Gordon',
-//     role: 'Office Manager',
-//     image: 'walter-gordon-office-manager.jpg',
-// }
-
-// const angelaLopez = {
-//     name: 'Angela Lopez',
-//     role: 'Social Media Manager',
-//     image: 'angela-lopez-social-media-manager.jpg',
-// }
-
-// const scottEstrada = {
-//     name: 'Scott Estrada',
-//     role: 'Developer',
-//     image: 'scott-estrada-developer.jpg',
-// }
-
-// const barbaraRamos = {
-//     name: 'Barbara Ramos',
-//     role: 'Graphic Designer',
-//     image: 'barbara-ramos-graphic-designer.jpg',
-// }
-
-
 const myTeam = [{
     name: 'Wayne Barnett',
     role: 'Founder & CEO',
