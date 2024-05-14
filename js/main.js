f813e88dadb8ae0a1fd85d33a413700d2a5cefb3
rinox3005 <peppe.rinox@gmail.com>
2024-05-14 15:58:38 +0200

    [FIX] unified name and lastname under a single key

--- js/main.js
+++ js/main.js
@@ -2,43 +2,37 @@
 
 // oggetti membri team
 const wayneBarnett = {
-    name: 'Wayne',
-    lastname: 'Barnett',
+    name: 'Wayne Barnett',
     role: 'Founder & CEO',
     image: 'wayne-barnett-founder-ceo.jpg',
 }
 
 const angelaCaroll = {
-    name: 'Angela',
-    lastname: 'Caroll',
+    name: 'Angela Caroll',
     role: 'Chief Editor',
     image: 'angela-caroll-chief-editor.jpg',
 }
 
 const walterGordon = {
-    name: 'Walter',
-    lastname: 'Gordon',
+    name: 'Walter Gordon',
     role: 'Office Manager',
     image: 'walter-gordon-office-manager.jpg',
 }
 
 const angelaLopez = {
-    name: 'Angela',
-    lastname: 'Lopez',
+    name: 'Angela Lopez',
     role: 'Social Media Manager',
     image: 'angela-lopez-social-media-manager.jpg',
 }
 
 const scottEstrada = {
-    name: 'Scott',
-    lastname: 'Estrada',
+    name: 'Scott Estrada',
     role: 'Developer',
     image: 'scott-estrada-developer.jpg',
 }
 
 const barbaraRamos = {
-    name: 'Barbara',
-    lastname: 'Ramos',
+    name: 'Barbara Ramos',
     role: 'Graphic Designer',
     image: 'barbara-ramos-graphic-designer.jpg',
 }
