e839f2d80a28f7977e82e6cd58758064a9ccafdf
rinox3005 <peppe.rinox@gmail.com>
2024-05-14 15:55:07 +0200

    [UPD] milestone 0 done

--- js/main.js
+++ js/main.js
@@ -1,2 +1,48 @@
 'use strict';
 
+// oggetti membri team
+const wayneBarnett = {
+    name: 'Wayne',
+    lastname: 'Barnett',
+    role: 'Founder & CEO',
+    image: 'wayne-barnett-founder-ceo.jpg',
+}
+
+const angelaCaroll = {
+    name: 'Angela',
+    lastname: 'Caroll',
+    role: 'Chief Editor',
+    image: 'angela-caroll-chief-editor.jpg',
+}
+
+const walterGordon = {
+    name: 'Walter',
+    lastname: 'Gordon',
+    role: 'Office Manager',
+    image: 'walter-gordon-office-manager.jpg',
+}
+
+const angelaLopez = {
+    name: 'Angela',
+    lastname: 'Lopez',
+    role: 'Social Media Manager',
+    image: 'angela-lopez-social-media-manager.jpg',
+}
+
+const scottEstrada = {
+    name: 'Scott',
+    lastname: 'Estrada',
+    role: 'Developer',
+    image: 'scott-estrada-developer.jpg',
+}
+
+const barbaraRamos = {
+    name: 'Barbara',
+    lastname: 'Ramos',
+    role: 'Graphic Designer',
+    image: 'barbara-ramos-graphic-designer.jpg',
+}
+
+
+const myTeam = [{ wayneBarnett }, { angelaCaroll }, { walterGordon }, { angelaLopez }, { scottEstrada }, { barbaraRamos }];
+console.log(myTeam);
