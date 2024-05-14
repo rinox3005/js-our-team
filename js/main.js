8b9910bdf14aec9603d9d05927f260fe3eba1d1c
rinox3005 <peppe.rinox@gmail.com>
2024-05-14 16:34:02 +0200

    [UPD] used different way to add object to array, printed in console name, role and img string

--- js/main.js
+++ js/main.js
@@ -1,42 +1,75 @@
 'use strict';
 
 // oggetti membri team
-const wayneBarnett = {
+// const wayneBarnett = {
+//     name: 'Wayne Barnett',
+//     role: 'Founder & CEO',
+//     image: 'wayne-barnett-founder-ceo.jpg',
+// }
+
+// const angelaCaroll = {
+//     name: 'Angela Caroll',
+//     role: 'Chief Editor',
+//     image: 'angela-caroll-chief-editor.jpg',
+// }
+
+// const walterGordon = {
+//     name: 'Walter Gordon',
+//     role: 'Office Manager',
+//     image: 'walter-gordon-office-manager.jpg',
+// }
+
+// const angelaLopez = {
+//     name: 'Angela Lopez',
+//     role: 'Social Media Manager',
+//     image: 'angela-lopez-social-media-manager.jpg',
+// }
+
+// const scottEstrada = {
+//     name: 'Scott Estrada',
+//     role: 'Developer',
+//     image: 'scott-estrada-developer.jpg',
+// }
+
+// const barbaraRamos = {
+//     name: 'Barbara Ramos',
+//     role: 'Graphic Designer',
+//     image: 'barbara-ramos-graphic-designer.jpg',
+// }
+
+
+const myTeam = [{
     name: 'Wayne Barnett',
     role: 'Founder & CEO',
     image: 'wayne-barnett-founder-ceo.jpg',
-}
-
-const angelaCaroll = {
+}, {
     name: 'Angela Caroll',
     role: 'Chief Editor',
     image: 'angela-caroll-chief-editor.jpg',
-}
-
-const walterGordon = {
+}, {
     name: 'Walter Gordon',
     role: 'Office Manager',
     image: 'walter-gordon-office-manager.jpg',
-}
-
-const angelaLopez = {
+}, {
     name: 'Angela Lopez',
     role: 'Social Media Manager',
     image: 'angela-lopez-social-media-manager.jpg',
-}
-
-const scottEstrada = {
+}, {
     name: 'Scott Estrada',
     role: 'Developer',
     image: 'scott-estrada-developer.jpg',
-}
-
-const barbaraRamos = {
+}, {
     name: 'Barbara Ramos',
     role: 'Graphic Designer',
     image: 'barbara-ramos-graphic-designer.jpg',
+}];
+
+console.log(myTeam);
+
+
+for (let i = 0; i < myTeam.length; i++) {
+    console.log('Name:', myTeam[i].name);
+    console.log('Role:', myTeam[i].role);
+    console.log('Image:', myTeam[i].image);
 }
 
-
-const myTeam = [{ wayneBarnett }, { angelaCaroll }, { walterGordon }, { angelaLopez }, { scottEstrada }, { barbaraRamos }];
-console.log(myTeam);
